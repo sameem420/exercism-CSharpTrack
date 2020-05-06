14ed85366fcf807a9755998912cd0775b212fb99
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-05-06 15:25:30 +0500

    2nd exercise solution

---
+++ pangram/Pangram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        char[] alphabets = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        input = input.ToLower();
+        for(int i = 0; i < alphabets.Length; i++)
+        {
+            if(!input.Contains(alphabets[i].ToString()))
+                return false;
+        }
+        return true;
+    }
+}
