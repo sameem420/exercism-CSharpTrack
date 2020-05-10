46e18c5b50a759df188fa4174a2a20b8d103b121
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-05-10 18:08:23 +0500

    acronym app added

---
+++ acronym/Acronym.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        char[] separators = { ' ', '-', '_' };
+        var words = phrase.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        string abberviation = "";
+        foreach (var word in words)
+        {
+            abberviation += word[0].ToString().ToUpper();
+        }
+        return abberviation;
+    }
+}
