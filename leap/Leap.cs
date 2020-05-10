cd438bf38abd9528cf95ad321bd56e783b37b902
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-05-10 18:18:17 +0500

    leap year program added

---
+++ leap/Leap.cs
@@ -0,0 +1,23 @@
+using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+        if(year % 4 == 0)
+        {
+            if(year % 100 != 0 || year % 400 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
