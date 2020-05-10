b76e0818209474f5d49bf9181b8dc92923650feb
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-05-10 18:03:00 +0500

    updated hello world app

---
+++ isogram/Isogram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        string subString = "";
+
+        word = word.ToLower().Replace("-", "").Replace(" ", "");
+        foreach (char ch in word)
+            if (!subString.Contains(ch))
+               subString += ch;
+            else 
+                return false;
+        return true;
+    }
+}
